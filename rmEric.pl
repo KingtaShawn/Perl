4e6db604c3e18dcd589fc88132c81c5fdbb973ac
Cao Jianhua <caojh2005@163.com>
2016-08-22 16:48:00 +0800

    Basic knowledge of Perl usage
    Usful for delect some comments

---
+++ rmEric.pl
@@ -0,0 +1,59 @@
+print "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+print "\n------------------ delect ericsson header ----------------\n";
+print "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+$inputFileName = $ARGV[0];
+print $inputFileName;
+chomp ($inputFileName);
+$outputFileName = $inputFileName;
+#$outputFileName =~ s/.dec//;
+$outputFileName1 = join($outputFileName,"baicells_","");
+print "\nOutput file is $outputFileName1\n";
+       
+open(inFile, "<$inputFileName")
+	or die "Couldn't open input file $inputFileName !\n";
+open(outFile1, ">$outputFileName1")
+	or die "Couldn't open output file $outputFileName1 !\n";
+
+while(<inFile>)
+{
+	$Line = $_;
+	if ($Line =~ /.* Description .*/)
+	{
+		$indFlag = 1;
+		next;
+	}
+	if ($Line =~ /.* Copyright Ericsson AB.*/)
+	{
+		$indFlag = 1;
+		next;
+	}
+	if ($Line =~ /.* The copyright to the computer .*/)
+	{
+		$indFlag = 1;
+		next;
+	}
+	if ($Line =~ /.* The programs may be used .*/)
+	{
+		$indFlag = 1;
+		next;
+	}
+	if ($Line =~ /.* Ericsson AB or in accordance .*/)
+	{
+		$indFlag = 1;
+		next;
+	}
+	if ($Line =~ /.*ntract under.*/)
+	{
+		$indFlag = 1;
+		next;
+	}
+	else
+	{
+		print outFile1 "$Line";
+	}	
+}	
+close(outFile1);
+close(inFile);
+
+
